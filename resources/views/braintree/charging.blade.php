80c694eed4d14ba2fb8c95811b6244f241927d4c
Luan Pham <luanpm88@gmail.com>
2021-04-28 00:11:31 +0700

    fix bug

--- resources/views/braintree/charging.blade.php
+++ resources/views/braintree/charging.blade.php
@@ -1,6 +1,6 @@
 <html lang="en">
     <head>
-        <title>{{ trans('cashier::messages.coinpayments.checkout.page_title') }}</title>
+        <title>{{ trans('cashier::messages.braintree.checkout.page_title') }}</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>    
         <style>
@@ -21,16 +21,16 @@
                 <div class="mb-40">
                     <img src="{{ \Acelle\Cashier\Cashier::public_url('images/loading.gif') }}" />
                 </div>
-                <h1 class="text-semibold mb-10">{!! trans('cashier::messages.coinpayments.checkout.processing_payment') !!}</h1>
+                <h1 class="text-semibold mb-10">{!! trans('cashier::messages.braintree.checkout.processing_payment') !!}</h1>
         
                 <div class="sub-section">
                     <div class="row">
                         <div class="col-md-12">
                             
                         
-                            <p class="text-muted">{!! trans('cashier::messages.coinpayments.checkout.processing_payment.intro') !!}</p>
+                            <p class="text-muted">{!! trans('cashier::messages.braintree.checkout.processing_payment.intro') !!}</p>
                             
-                            <form id="pay_now" method="POST" action="{{ \Acelle\Cashier\Cashier::lr_action('\Acelle\Cashier\Controllers\CoinpaymentsController@checkout', [
+                            <form id="pay_now" method="POST" action="{{ \Acelle\Cashier\Cashier::lr_action('\Acelle\Cashier\Controllers\BraintreeController@checkout', [
                                 'invoice_uid' => $invoice->uid,
                             ]) }}">
                                 {{ csrf_field() }}
