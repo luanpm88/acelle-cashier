f003b8fe1373f5e34f224b4dc737b4029fd47c50
Luan Pham <luanpm88@gmail.com>
2022-11-09 22:19:16 +0700

    remove icon

--- resources/views/coinpayments/pending.blade.php
+++ resources/views/coinpayments/pending.blade.php
@@ -114,9 +114,6 @@ display: block;" href="{{ $service->getData($invoice)['status_url'] }}">
         <div class="col-md-4">
             <div class="card shadow-sm rounded-3 px-2 py-2 mb-4">
                 <div class="card-body p-4">
-                    <span class="material-symbols-rounded card-top-left-icon">
-                        receipt_long
-                    </span>
                     @include('invoices.bill', [
                         'bill' => $invoice->getBillingInfo(),
                     ])
