72c0be8897be83cc2ffe38513874a165633cefad
Support <support@b-teka.com>
2022-12-11 16:33:24 +0700

    Japanese translation

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,154 @@
+<?php return array (
+  'braintree' => 'Braintree',
+  'braintree.card_list' => 'カード情報',
+  'braintree.checkout.page_title' => 'チェックアウト',
+  'braintree.checkout.processing_payment' => 'お支払いを処理しています...お待ちください！',
+  'braintree.checkout.processing_payment.intro' => 'このプロセスは自動的に行われます。 このブラウザ/タブを閉じたり、ページを変更したりしないでください。 <br> お使いのブラウザはまもなく次のページにリダイレクトされます。',
+  'braintree.checkout_with_braintree' => 'ブレインツリーでチェックアウト',
+  'braintree.click_bellow_to_pay' => 'Braintree で <strong>:price</strong> の <strong>:plan</strong> をチェックアウトしています。 以下のカード情報を見つけて、[支払う] ボタンをクリックして支払いを続行します。',
+  'braintree.current_card' => '現在のカード',
+  'braintree.description' => 'クレジット/デビットカードからブレインツリーアカウントへの支払いを受け取ります',
+  'braintree.environment' => '環境',
+  'braintree.intro' => 'Braintree は、オンライン決済を簡単に受け入れることができるフルスタックの決済プラットフォームです。 Braintree アカウントにサインアップし、統合 ID とキーを取得して、以下で設定する必要があります。',
+  'braintree.merchant_id' => 'Merchant ID',
+  'braintree.new_card' => 'New card',
+  'braintree.pay' => 'Pay',
+  'braintree.pay_with_new_card' => 'Pay with new card',
+  'braintree.pay_with_this_card' => 'Pay with this card',
+  'braintree.pending.page_title' => 'Transaction is pending',
+  'braintree.private_key' => 'Private key',
+  'braintree.public_key' => 'Public key',
+  'braintree.short_description' => 'Pay for your subscription & services using your debit
+        or credit cards. Auto billing is supported',
+  'braintree.use_new_card' => 'Use new card',
+  'braintree.use_this_card' => 'Use this card',
+  'cancel' => 'Cancel',
+  'card.brand' => 'Brand',
+  'card.last4' => 'Last 4 number',
+  'coinpayments' => 'Coin-Payments',
+  'coinpayments.amount' => 'Amount',
+  'coinpayments.checkout.processing_payment' => 'Processing your payment... please wait!',
+  'coinpayments.checkout.processing_payment.intro' => 'このプロセスは自動的に行われます。 このブラウザ/タブを閉じたり、ページを変更したりしないでください。
+ <br> ブラウザはまもなく次のページにリダイレクトされます。',
+  'coinpayments.checkout_url' => 'Checkout URL',
+  'coinpayments.description' => 'Receive payment from a cryptocurrency like Bitcoin, Monero, ZCash, etc.',
+  'coinpayments.intro' => 'You can accept payments from many cryptocurrencies like Bitcoin, Monero, ZCash, etc.
+        You will need to sign up for a Coin-Payments account, obtain your integration ID and keys and set it up below',
+  'coinpayments.ipn_secret' => 'IPN secret',
+  'coinpayments.list_intro' => 'Receive payment from a cryptocurrency like Bitcoin, Monero, ZCash, etc.',
+  'coinpayments.merchant_id' => 'Merchant ID',
+  'coinpayments.next_period_day' => 'Next period end day',
+  'coinpayments.pending.intro' => 'You have unpaid invoice.
+        Click on the <strong>checkout url</strong> to pay or <strong>status url</strong> below to pay/check your payment status',
+  'coinpayments.plan' => 'Plan',
+  'coinpayments.private_key' => 'Private key',
+  'coinpayments.public_key' => 'Public key',
+  'coinpayments.receive_currency' => 'Receive Currency Code',
+  'coinpayments.short_description' => 'Pay for your subscription & services with a cryptocurrency, via coinpayment.com',
+  'coinpayments.status' => 'Status',
+  'coinpayments.status_code' => 'Status code',
+  'coinpayments.status_url' => 'Status URL',
+  'coinpayments.wording' => 'You can accept payments from many cryptocurrencies like Bitcoin, Monero, ZCash, etc. You will need to sign up for a Coin-Payments account, obtain your integration ID and keys and set it up below',
+  'connect' => 'Connect',
+  'gateway.updated' => 'Gateway settings were updated',
+  'go_back' => '戻る',
+  'offline' => 'Offline',
+  'offline.claim_payment' => '請求の支払い',
+  'offline.description' => 'Receive payments outside of the application',
+  'offline.payment_instruction' => 'Payment Instruction',
+  'offline.payment_instruction.default' => '
+        <p>Please make a deposit to our bank account at:</p>
+
+        <p><strong>FIRST CENTURY BANK USA</strong><br>
+        Routing (ABA): 061120084<br>
+        Account number: 4013000067378<br>
+        Beneficiary name: Jolie Kennedy<br>
+        </p>
+
+        <p>---</p>
+        
+        <p><strong>CITIBANK UK</strong><br>
+        SWIFT CODE: CIBLBDDHXXX<br>
+        Account number: 9000110067378<br>
+        Beneficiary name: Jolie Kennedy<br>
+        </p>
+        
+        <p class="text-danger" style="color: red">* Above is sample payment information, you can change it in your payment gateway setting page</p>
+    ',
+  'offline.short_description' => 'チェックアウト時に提供される指示に従って、アプリケーション外でサブスクリプションとサービスの支払いを行います',
+  'pay_invoice' => '請求書の支払い',
+  'payment.options' => 'Payment Options',
+  'paypal' => 'Paypal',
+  'paypal.checkout.intro' => 'You are paying an invoice with amount <strong>:price</strong>.
+        Click on the button below to finish your payment with PayPal before using plan.',
+  'paypal.checkout.processing_payment' => 'Processing your payment... please wait!',
+  'paypal.checkout.processing_payment.intro' => 'This process is automatic. Please do not close this browser/tab or change page.
+        <br> Your browser will redirect to next page shortly.',
+  'paypal.client_id' => 'Client ID',
+  'paypal.description' => 'PayPal is the fast/safe way to send money, make an online payment, receive money or set up a merchant account',
+  'paypal.environment' => 'Environment',
+  'paypal.intro' => 'PayPal is the fast/safe way to send money, make an online payment, receive money or set up a merchant account.',
+  'paypal.secret' => 'Secret',
+  'paypal.short_description' => 'PayPal アカウントを使用して、サブスクリプションとサービスの料金を支払います。',
+  'paypal.user.description' => 'Pay for your subscription & services using your PayPal account.',
+  'paystack' => 'Paystack',
+  'paystack.checkout.processing_payment' => 'Processing your payment... please wait!',
+  'paystack.checkout.processing_payment.intro' => 'This process is automatic. Please do not close this browser/tab or change page.
+        <br> Your browser will redirect to next page shortly.',
+  'paystack.checkout_with_paystack' => 'Pay with Paystack',
+  'paystack.click_bellow_to_pay' => 'You are paying an invoice with amount <strong>:price</strong>.
+        Please click the button bellow to proceed with the payment information.',
+  'paystack.description' => 'Receive payments from Credit / Debit card to your Paystack account',
+  'paystack.intro' => 'Start Accepting Payments Instantly with Razorpay\'s Free Payment Gateway.
+        Supports Netbanking, Credit, Debit Cards etc',
+  'paystack.pay' => 'Pay Now',
+  'paystack.public_key' => 'Public key',
+  'paystack.secret_key' => 'Secret key',
+  'paystack.short_description' => 'Pay for your subscription & services using your debit
+        or credit cards. Auto billing is supported',
+  'razorpay' => 'Razorpay',
+  'razorpay.checkout.intro' => 'You are paying an invoice with amount <strong>:price</strong>.
+        Click on the button below to finish your payment with PayPal before using plan.',
+  'razorpay.checkout.processing_payment' => 'Processing your payment... please wait!',
+  'razorpay.checkout.processing_payment.intro' => 'This process is automatic. Please do not close this browser/tab or change page.
+        <br> Your browser will redirect to next page shortly.',
+  'razorpay.description' => 'Start Accepting Payments Instantly with Razorpay\'s
+        Free Payment Gateway. Supports Netbanking, Credit, Debit Cards etc',
+  'razorpay.intro' => 'Start Accepting Payments Instantly with Razorpay\'s Free Payment Gateway. Supports Netbanking, Credit, Debit Cards etc',
+  'razorpay.key_id' => 'Key ID',
+  'razorpay.key_secret' => 'Key Secret',
+  'razorpay.pay_with_razorpay' => 'Pay with Razorpay',
+  'razorpay.short_description' => 'Pay for your subscription & services using your debit
+        or credit cards. Auto billing is not supported',
+  'save_and_enable' => 'Save & Enable',
+  'stripe' => 'Stripe',
+  'stripe.add_card' => 'Add card',
+  'stripe.card_list' => 'Your card information',
+  'stripe.change_card' => 'Change card',
+  'stripe.checkout.page_title' => 'Checkout',
+  'stripe.checkout.processing_payment' => 'Processing your payment... please wait!',
+  'stripe.checkout.processing_payment.intro' => 'This process is automatic. Please do not close this browser/tab or change page.
+        <br> Your browser will redirect to next page shortly.',
+  'stripe.checkout_with_stripe' => 'Checkout with Stripe',
+  'stripe.click_button_to_connect' => 'Click button below to add payment method',
+  'stripe.click_to_auth' => 'Click <a href=":link">here</a> to authenticate the payment.',
+  'stripe.connected' => 'You connected to Stripe successfully',
+  'stripe.current_card' => 'Your current card',
+  'stripe.description' => 'Receive payments from Credit / Debit card to your Stripe account',
+  'stripe.intro' => 'Stripe is a technology company based in San Francisco, California.
+        Its software allows individuals and businesses to make and receive payments over the Internet.
+        Stripe provides the technical, fraud prevention, and banking infrastructure required to
+        operate online payment systems.',
+  'stripe.new_card' => 'New Card',
+  'stripe.new_card.intro' => 'Fill your card information below and click Pay button.',
+  'stripe.no_card' => 'Fill your card information below and click Pay button.',
+  'stripe.pay' => 'Pay',
+  'stripe.pay_with_new_card' => 'Pay with new card',
+  'stripe.pay_with_this_card' => 'Pay with this card',
+  'stripe.publishable_key' => 'Publishable key',
+  'stripe.return_back' => 'Return back',
+  'stripe.secret_key' => 'Secret key',
+  'stripe.short_description' => 'Pay for your subscription & services using your debit
+        or credit cards. Auto billing is supported',
+  'stripe.use_current_card' => 'Use current card',
+) ?>
